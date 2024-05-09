16db72bc58815d2ba74a5e1fe5f4cd80a27e8630
Vyacheslav <slava.shilin07@mail.ru>
2024-05-09 14:44:09 +0300

    Finished workflow

--- .github/workflows/documentation_update.py
+++ .github/workflows/documentation_update.py
@@ -1,13 +1,6 @@
 import os
 import re
 
-print("Hello, World!")
-print("Hello, World!")
-print("Hello, World!")
-print("Hello, World!")
-print("Hello, World!")
-print("Hello, World!")
-
 
 class bcolors:
     HEADER = '\033[95m'
@@ -21,66 +14,93 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-def find_controllers(directory):
-    controllers = []
+def find_controllers():
     for root, dirs, files in os.walk(directory):
         for file in files:
-            if file.endswith(".cs"):  # Предполагаем, что контроллеры имеют расширение .cs
-                file_path = os.path.join(root, file)
-                with open(file_path, 'r', encoding='utf-8') as f:
-                    content = f.read()
-                    if '[Controller]' in content:
-                        controllers.append(file_path)
-    return controllers
+            if not file.endswith(".cs"): continue
+            file_path = os.path.join(root, file)
+            with open(file_path, 'r', encoding='utf-8') as f:
+                content = f.read()
+                if '[Controller]' in content:
+                    controllers.append(file_path)
 
 
 def extract_info(file_path):
     info = {}
     with open(file_path, 'r', encoding='utf-8') as f:
-        lines = f.readlines()
-        for line in lines:
-            if "public string Route" in line:
-                info["Route"] = line.split('=>')[1].strip().strip('"')
-            elif "public string HttpMethod" in line:
-                info["HttpMethod"] = line.split('=>')[1].strip().strip('"')
-            elif "public string Description" in line:
-                info["Description"] = line.split('=>')[1].strip().strip('"')
+        content = f.read()
+
+        info["Route"] = re.search(r'public string Route\s*=>\s*"([^"]+)"', content).group(1)
+        info["HttpMethod"] = re.search(r'public string HttpMethod\s*=>\s*"([^"]+)"', content).group(1)
+        info["Description"] = re.search(r'public string Description\s*=>\s*"([^"]+)"', content).group(1)
+
+        # Извлечение QueryParameters
+        query_parameters_match = re.search(r'public List<QueryParamInfo>\s*QueryParameters\s*=>\s*\[([^]]*)]',
+                                           content, re.DOTALL)
+        if query_parameters_match:
+            query_parameters_content = query_parameters_match.group(1)
+            if query_parameters_content.strip() == "":  # Проверяем, пуст ли список QueryParameters
+                info["QueryParameters"] = []
+            else:
+                query_parameters = []
+                query_parameter_matches = re.finditer(r'new\(\s*"([^"]+)"\s*,\s*"([^"]+)"\s*,\s*"([^"]+)"\s*\)',
+                                                      query_parameters_content)
+                for match in query_parameter_matches:
+                    query_parameters.append(
+                        {"name": match.group(1), "type": match.group(2), "description": match.group(3)})
+                info["QueryParameters"] = query_parameters
+        else:
+            info["QueryParameters"] = []
     return info
 
 
-def generate_readme_entries(controllers):
-    readme_entries = []
+def generate_readme_entries():
     for controller in controllers:
         info = extract_info(controller)
-        filename = os.path.basename(controller)
+        # filename = os.path.basename(controller)
         if all(key in info for key in ["Route", "HttpMethod", "Description"]):
-            entry = f"### {filename}\n{info['HttpMethod']} {info['Route']} - {info['Description']}"
+            entry = (
+                f'### {info['Route']}\n'
+                f'Description: {info['Description']}<br>\n'
+                f'HttpMethod: {info['HttpMethod']}<br>\n')
+
+            query_parameters = info["QueryParameters"]
+            if query_parameters:
+                entry += 'Query parameters\n'
+                for query_parameter in query_parameters:
+                    param_name = query_parameter["name"]
+                    param_type = query_parameter["type"]
+                    param_description = query_parameter["description"]
+                    entry += f'* {param_name}<br>\n'
+                    entry += f'    Description: {param_description}<br>\n'
+                    entry += f'    Type: {param_type}<br>\n'
+                    entry += f'\n'
+            else:
+                entry += 'Has no query parameters\n'
+
             readme_entries.append(entry)
-    return readme_entries
 
 
-def update_readme(readme_path, readme_entries):
+def update_readme():
     with open(readme_path, 'w', encoding='utf-8') as f:
+        f.write(f'# Documentation\n')
+        f.write(f'## Controllers\n')
         for entry in readme_entries:
             f.write(entry + '\n')
 
 
+directory = os.getcwd() + "/mod/Web/Controllers"
+readme_path = "DOCUMENTATION.md"
+controllers = []
+readme_entries = []
 if __name__ == "__main__":
-    # Путь к директории с контроллерами
-    directory = os.getcwd() + "/mod/Web/Controllers"
-    readme_path = "README.md"  # Путь к README файлу
-
     print('Looking for new controllers in directory', directory)
 
-    controllers = find_controllers(directory)
-    readme_entries = generate_readme_entries(controllers)
-    update_readme(readme_path, readme_entries)
+    # clean readme file
+    with open(readme_path, 'w', encoding='utf-8') as f:
+        f.write("# Documentation\n## Controllers\n")
+    print('Cleaned readme file -', readme_path, f'Content: {open(readme_path, "r", encoding="utf-8").read()}')
 
-
-# if __name__ == "__main__":
-#     directory = os.getcwd()
-#     print('Looking for new todos in directory', directory)
-#     new_todo_pattern = re.compile(r"//TODO:\s*([^\n]+)")
-#     old_todo_pattern = re.compile(r'#+\s*TODOs(.*?)(?=\n?#+|$)', re.DOTALL)
-#     readme_path = os.path.join(directory, 'README.md')
-#     overwrite_todos(find_new_todos(['.cs']))
+    find_controllers()
+    generate_readme_entries()
+    update_readme()
